1caebe153e94edda255a34e7c2f5054c24fa0885
Owen Holloway <owen.holloway@lanceolata.com.au>
2020-12-12 11:21:30 +1100

    Init project creation

---
+++ src/Saraswati/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saraswati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
